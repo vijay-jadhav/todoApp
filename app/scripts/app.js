6aaa617a171cb5c373bdb8b2b4e3a725e2a214f4
unknown <vijayj@ULTP_146.synerzipune.local>
2015-01-22 17:14:10 +0530

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,23 @@
+angular.module("todoApp", [])
+    .controller("MainCtrl", function($scope) {
+        console.log("in Main controller..");
+
+        $scope.todos = [];
+        $scope.add = function() {
+            $scope.todos.push($scope.newTodo);
+            $scope.newTodo = {};
+        }
+        $scope.delete = function(todo) {
+            var indexOfTodo = $scope.todos.indexOf(item);
+            $scope.todos.splice(indexOfTodo, 1);
+        }
+
+        $scope.clearCompleted = function() {
+            $scope.todos.forEach(function(todo) {
+
+                if(todo.completed) {
+                    $scope.delete(todo);
+                }
+             });
+        }
+    });
